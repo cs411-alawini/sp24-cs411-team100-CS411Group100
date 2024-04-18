6bdc6784d01d7aab72daa56ba5017f79f837a601
VimalnathAchuthan <157620739+VimalnathAchuthan@users.noreply.github.com>
2024-04-18 03:21:46 -0500

    implemented transaction summary and amount transfer apis

--- Backend/routers/api.js
+++ Backend/routers/api.js
@@ -3,6 +3,7 @@ const healthChecker = require('../healthChecker')
 const authRouter= require("./auth")
 const userRouter= require("./user")
 const accountRouter= require("./account")
+const transactionRouter= require("./transaction")
 
 var router = express.Router();
 
@@ -11,5 +12,6 @@ router.get('/health', healthChecker.checkStatus)
 router.use('/auth', authRouter)
 router.use('/user', userRouter)
 router.use('/account', accountRouter)
+router.use('/transaction', transactionRouter)
 
 module.exports = router;
