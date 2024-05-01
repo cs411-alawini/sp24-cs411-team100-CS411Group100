2338617331408ada4edfdcb9905913ad3eb3b91a
Muhammad Huzaifa Khan Suri <mhsuri2@illinois.edu>
2024-05-01 01:21:09 -0500

    Add logo and background gradient

--- Frontend/src/components/HeaderAccount.js
+++ Frontend/src/components/HeaderAccount.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Header.css';
+import '../styles/HeaderAccount.css';
+import logoImage from '../assets/logo_white.png';
 
-function HeaderAccount() {
+function HeaderAccount({ logoWidth = '60px' }) {
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -16,7 +17,9 @@ function HeaderAccount() {
 
   return (
     <div className="header">
-      <div className="logo">Logo</div>
+      <div className="logo-header">
+      <img src={logoImage} alt="DollarIQ" style={{ width: logoWidth }} />
+      </div>
       <div className="navigation">
         <button className="nav-button" onClick={goToSettings}>Settings</button>
         <button className="nav-button" onClick={handleLogout}>Log Out</button>
--- Frontend/src/components/LoginChoice.js
+++ Frontend/src/components/LoginChoice.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
+import { useNavigate } from 'react-router-dom';
 import '../styles/LoginChoice.css';
+import logoImage from '../assets/logo.png'; // Assuming the logo is stored in src/assets
 
 function LoginChoice() {
-  let navigate = useNavigate(); // useNavigate instead of useHistory
+  let navigate = useNavigate();
 
   const navigateToLogin = (userType) => {
     navigate(`/login/${userType}`);
@@ -11,7 +12,9 @@ function LoginChoice() {
 
   return (
     <div className="login-choice">
-      <div className="Logo">DollarIQ</div>
+      <div className="logo">
+        <img src={logoImage} alt="DollarIQ" />
+      </div>
       <button onClick={() => navigateToLogin('admin')} className="login-button">Admin Login</button>
       <button onClick={() => navigateToLogin('user')} className="login-button">User Login</button>
     </div>
